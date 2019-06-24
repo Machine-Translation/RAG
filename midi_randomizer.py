a7a2d9aa21f99b497679d0d727ae4b7057056367
Cory Edwards <14corman@gmail.com>
2019-06-23 20:02:57 -0500

    more work on midi_randomizer
    I made more functions to make the file more useful for us. It can now create examples of output given a level, create a certain number of output files (txt), convert the text files to midi and convert back. Also fixed a few bugs and added "smart" randomization where the code uses the tempo to create enough notes in the song file to either be +-10 seconds away from 1 minutes worth of music.

--- midi_randomizer.py
+++ midi_randomizer.py
@@ -8,6 +8,9 @@ Created on Sat Jun 22 20:00:10 2019
 
 from music21 import converter, instrument, chord, stream, tempo, note
 import numpy as np
+import glob
+import re
+import os
 
 def create_note(length, is_rest):
     """Creates a music21 note based on length and whether it is a rest or not"""
@@ -206,7 +209,7 @@ def create_rand_notes(level, tempo_time):
         distribution.append(prob)
         sum += prob
 
-    print(sum)
+    #print(sum)
 
     # Using the tempo, we will get enough of these notes to come within +-10 seconds of a
     # minutes worth of notes.
@@ -227,22 +230,23 @@ def create_rand_notes(level, tempo_time):
 
     return rand_notes
 
-def create_rand_midi(notes, tempo_time, file_name):
-    """Create a random music track from midi given a music level"""
+def create_midi(notes, tempo_time, file_name):
+    """Given an array of notes, a tempo to use, and a file name, create a midi music track"""
     output_notes = []
     output_notes.append(tempo.MetronomeMark(number=tempo_time))
     int_to_note = get_note_dic(True)
     offset = 0.0
     for id in notes:
-        length, is_rest = int_to_note.get(id)
-        new_note = create_note(length, is_rest)
-        new_note.offset = offset
-        output_notes.append(new_note)
+        if int_to_note.get(id) != "|":
+            length, is_rest = int_to_note.get(id)
+            new_note = create_note(length, is_rest)
+            new_note.offset = offset
+            output_notes.append(new_note)
 
-        # increase offset each iteration so that notes do not stack
-        offset += new_note.duration.quarterLength
+            # increase offset each iteration so that notes do not stack
+            offset += new_note.duration.quarterLength
 
-    print(offset)
+    #print(offset)
     midi_stream = stream.Stream(output_notes)
     midi_stream.write('midi', fp='{}.mid'.format(file_name))
 
@@ -254,8 +258,8 @@ def create_text_file(notes, tempo_time, file_name):
         file.write(str(tempo_time) + ":")
         file.write("\t".join(str_notes))
 
-
-if __name__ == '__main__':
+def print_notes():
+    """Print out the complete list of notes and their ids"""
     for id, n in get_note_dic(True).items():
         print("Note id: " + str(id))
         if id == 0:
@@ -268,8 +272,101 @@ if __name__ == '__main__':
 
         print()
 
-    level = 10
+def create_example(level):
+    """
+    Create an example with a random tempo and random set up notes based on
+    a given level. This makes both the midi file and the corresponding text output
+    file that the program would use.
+    """
     rand_tempo = create_rand_tempo()
     rand_notes = create_rand_notes(level, rand_tempo)
-    create_rand_midi(rand_notes, rand_tempo, "random_{}".format(level))
+    create_midi(rand_notes, rand_tempo, "random_{}".format(level))
     create_text_file(rand_notes, rand_tempo, "random_{}".format(level))
+
+def create_output_files(level, num_files):
+    """
+    Create a set of text files that the program will use to play the game and
+    the ML algorithm will use to train on.
+    """
+    for file in range(num_files):
+        rand_tempo = create_rand_tempo()
+        rand_notes = create_rand_notes(level, rand_tempo)
+        create_text_file(rand_notes, rand_tempo, "{}_{}".format(level, file))
+
+def convert_text_to_midi(dir):
+    """
+    Takes a directory of txt files that are correctly formatted, and creates midi
+    files based off of them.
+    """
+    for glob_file in glob.glob("{}/*.txt".format(dir)):
+        print(glob_file)
+        with open(glob_file, "r") as file:
+            text = file.read()
+
+        if re.match(r"^\d+:(\d+\t)+0$", text):
+            file_name = os.path.split(glob_file)[1].split(".")[0]
+            parts = text.split(":")
+            tempo_time = int(parts[0])
+            notes = [int(x) for x in parts[1].split("\t")]
+            create_midi(notes, tempo_time, file_name)
+
+
+
+def convert_midi_to_text(dir):
+    """
+    Takes a directory of midi files, and creates text files based off of them.
+    """
+    note_to_int = get_note_dic(False)
+    for glob_file in glob.glob("{}/*.mid".format(dir)):
+        midi = converter.parse(glob_file)
+        notes = []
+        tempo_time = 0
+        file_name = os.path.split(glob_file)[1].split(".")[0]
+
+        print(glob_file)
+
+        try: # file has instrument parts
+            s2 = instrument.partitionByInstrument(midi)
+            score = s2.parts[0].recurse() #.makeRests(fillGaps=True, inPlace=False)
+            #tempo_time = s2.flat.getElementsByClass(tempo.MetronomeMark)[0].number
+        except: # file has notes in a flat structure
+            score = midi.flat.recurse()
+            #tempo_time = midi.flat.getElementsByClass(tempo.MetronomeMark)[0].number
+
+        def set_note(element, is_rest):
+            note_parts = element.splitAtDurations()
+            for np in note_parts:
+                temp_length = np.duration.quarterLength
+                num_whole_notes = 0
+                while temp_length > 4.0:
+                    temp_length -= 4.0
+                    num_whole_notes += 1
+
+                if num_whole_notes > 0:
+                    notes.append(note_to_int.get((temp_length, is_rest)))
+                    for _ in range(num_whole_notes):
+                        notes.append(note_to_int.get((4.0, is_rest)))
+                else:
+                    #print(np.duration.type)
+                    #print(np.offset)
+                    #print((temp_length, is_rest))
+                    #print(note_to_int.get((temp_length, is_rest)))
+                    notes.append(note_to_int.get((temp_length, is_rest)))
+
+        for element in score:
+            if isinstance(element, note.Note):
+                set_note(element, False)
+            elif isinstance(element, note.Rest):
+                set_note(element, True)
+            elif isinstance(element, tempo.MetronomeMark):
+                tempo_time = int(element.number)
+
+        create_text_file(notes, tempo_time, file_name)
+
+
+if __name__ == '__main__':
+    #print_notes()
+    #create_example(10)
+    #create_output_files(1, 15)
+    #convert_text_to_midi(".")
+    convert_midi_to_text(".")
